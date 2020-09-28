13575e0f2e8e1b71aa34d49dbff22ac72316a6e4
ameyvedpathak <ameyv23@gmail.com>
2020-09-28 16:38:51 +0100

    Created "Simplified-Weather-Application-Infrastructure-State"bucket

--- s3module/s3.tf
+++ s3module/s3.tf
@@ -3,6 +3,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+ backend "s3" {
+   bucket = "Simplified-Weather-Application-Infrastructure-State"
+   key    = "default-infrastructure"
+   region = "us-east-1"
+ }
+}
+
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "localopenweatherdata-test"
 
