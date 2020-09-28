059a82ab11d2735062b03f0734ca345291e11501
ameyvedpathak <ameyv23@gmail.com>
2020-09-28 18:49:29 +0100

    added lambda function

--- main.tf
+++ main.tf
@@ -20,3 +20,56 @@ resource "aws_s3_bucket" "s3_bucket_2" {
   bucket = "simplifiedweatherapp"
 
 }
+
+/*
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+   region = "us-east-1"
+}
+*/
+
+resource "aws_lambda_function" "example" {
+   function_name = "queryopenweatherapi"
+
+   # "main" is the filename within the zip file (main.js) and "handler"
+   # is the name of the property under which the handler function was
+   # exported in that file.
+   handler = "lambda_function.lambda_handler"
+   runtime = "python3.8"
+
+   role = aws_iam_role.lambda_exec.arn
+}
+
+ # IAM role which dictates what other AWS services the Lambda function
+ # may access.
+resource "aws_iam_role" "lambda_exec" {
+   name = "queryopenweatherapi-role-jjq8zbll"
+
+   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+resource "aws_iam_role_policy_attachment" "lambda_exec-attach" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::308726405065:role/service-role/queryopenweatherapi-role-jjq8zbll"
+}
