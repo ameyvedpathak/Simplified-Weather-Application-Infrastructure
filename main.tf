0e30b8f078a913748a442e631766265fed06e99a
ameyvedpathak <ameyv23@gmail.com>
2020-10-02 12:15:22 +0100

    edited 9

--- main.tf
+++ main.tf
@@ -39,21 +39,17 @@ resource "aws_s3_bucket_policy" "s3_bucket_2" {
   bucket = aws_s3_bucket.s3_bucket_2.id
 
   policy = <<POLICY
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-          {
-              "Sid": "PublicReadGetObject",
-              "Effect": "Allow",
-              "Principal": "*",
-              "Action": [
-                  "s3:GetObject"
-              ],
-              "Resource": [
-                  "arn:aws:s3:::aws_s3_bucket.s3_bucket_2.bucket/*"
-              ]
-          }
-      ]
+  {
+    "Version":"2012-10-17",
+    "Statement":[
+      {
+        "Sid":"PublicRead",
+        "Effect":"Allow",
+        "Principal": "*",
+        "Action":["s3:GetObject","s3:GetObjectVersion"],
+        "Resource":["arn:aws:s3:::simplifiedweatherapp-test/*"]
+      }
+    ]
   }
 POLICY
 
